06d54d531ce4af2fd93e3cb04e25995c538fcf56
xknolls <75128806+xknolls@users.noreply.github.com>
2021-01-21 16:32:32 +0100

    cours du jour

--- mudule2/php/csv/step3/delete.php
+++ mudule2/php/csv/step3/delete.php
@@ -1,3 +1,93 @@
 <?php
 //Traitement du formulaire permettant de supprimer les contacts selectionés
-var_dump($_POST) ;
+var_dump($_POST);
+
+const DATAFILE = 'data/contacts.csv';
+
+
+/*
+    Si on veut supprimer une ligne dans un tableau
+
+    Si le formulaire a été posté et que des valeurs sont à supprimer
+    Alors : 
+        Charger les contactes stockées dans le fichier dans un tableau
+        Pour chaque ligne du tableau de contacts
+            Si son index se trouve dans les indexs à supprimer :
+            Alors : 
+                Supprimer la ligne correspondant à cet index dans le tableau des conacts
+            Si le fichier CSV est accessible en écriture
+            Alors :
+                Ouvrir le fichier csv en ecriture en placant le pointeur au debut et en réduisant le fichier à 0 pour le ré-écrire
+                Tant qu'il y a des lignes dans le tableau de contacts restants 
+                    Insérer la ligne dans le fichier CSV
+                Fermer le fichier
+            Rediriger vers l'index
+
+    Si on ne veut pas supprimer une ligne dans le tableau
+*/
+if (!array_key_exists('todelete', $_POST) == true) {
+    header('location: index.php');
+}
+$contacts = array();
+
+if (is_readable(DATAFILE)) {
+    $file = fopen(DATAFILE, 'r');
+
+    while ($data = fgetcsv($file, 0, ';')) {
+        array_push($contacts, $data);
+    }
+
+    fclose($file);
+}
+
+foreach ($contacts as $key => $value) {
+    if (in_array($key, $_POST['todelete'])) {
+        unset($contacts[$key]);
+    }
+}
+$file = fopen(DATAFILE, 'w+');
+
+foreach ($contacts as $line) {
+    fputcsv($file, $line, ';');
+}
+
+fclose($file);
+
+header('Location: index.php');
+
+
+
+
+
+
+
+
+
+/*
+    Si le formulaire a été posté et que des valeurs sont à supprimer
+    Alors : 
+        Créer un nouveau tableau : new_contacts
+        Charger les contactes stockées dans le fichier dans un tableau contacts
+        Pour chaque ligne du tableau de contacts
+            Si son index ne se trouve pas dans les index à supprimer
+                Ajouter la ligne dans le tableau new_contacts
+    Si le fichier CSV est accessible en écriture
+            Alors :
+                Ouvrir le fichier csv en ecriture en placant le pointeur au debut et en réduisant le fichier à 0 pour le ré-écrire
+                Tant qu'il y a des lignes dans le tableau de contacts restants 
+                    Insérer la ligne dans le fichier CSV
+                Fermer le fichier
+            Rediriger vers l'index
+*/
+
+if (!empty($_POST)) {
+
+    $new_contacts = array();
+
+    $contacts = array(
+        'firstname' => '',
+        'lastname' => '',
+        'email' => '',
+        'tel' => ''
+    );
+}
--- mudule2/php/intro/array.php
+++ mudule2/php/intro/array.php
@@ -4,6 +4,7 @@ ini_set("display_errors", 1);
 ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,87 +14,88 @@ ini_set("display_errors", 1);
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/styles.css">
 </head>
+
 <body>
     <header class="banner">
         <a href="site-logo" href="./">PHP</a>
         <p class="site-description">Description du TP</p>
-        <?php include'include/menu.php'; ?>
+        <?php include 'include/menu.php'; ?>
     </header>
     <main>
         <h1>Variable Tableau/Array</h1>
         <?php
-            //Stocker le prénom de chaque stagiaire dansune variable
-            
-            $greta2021 = ['Véro','Quentin','Fils de pute','Jenny','Déborah','Renaud','Loïc','Brahim','Léo','Christopher'];
+        //Stocker le prénom de chaque stagiaire dansune variable
 
-            echo '<p>My name is ' .$greta2021[1]. '<p>';
+        $greta2021 = ['Véro', 'Quentin', 'Fils de pute', 'Jenny', 'Déborah', 'Renaud', 'Loïc', 'Brahim', 'Léo', 'Christopher'];
 
-            var_dump($greta2021);
+        echo '<p>My name is ' . $greta2021[1] . '<p>';
+
+        var_dump($greta2021);
         ?>
         <?php
-            echo '<p> La session compte ' . count($greta2021) . ' stagiaires</p>';
+        echo '<p> La session compte ' . count($greta2021) . ' stagiaires</p>';
         ?>
         <ol>
-            <?php for($compteur = 0; $compteur < 10; $compteur++ ) : ?>
-            <li><?= $greta2021[$compteur]?></li>
+            <?php for ($compteur = 0; $compteur < 10; $compteur++) : ?>
+                <li><?= $greta2021[$compteur] ?></li>
             <?php endfor; ?>
         </ol>
 
-        <?php 
-            //Utiliser une seule variable pour stocker les informations d'une photo
-            $picture = [];
-            $picture[] = 'img/alicia_720.png';
-            $picture[] = 'Juste la bosse enfaite';
+        <?php
+        //Utiliser une seule variable pour stocker les informations d'une photo
+        $picture = [];
+        $picture[] = 'img/alicia_720.png';
+        $picture[] = 'Juste la bosse enfaite';
         ?>
         <img src="<?= $picture[0] ?>" alt="<?= $picture[1] ?>" width="200">
-        
+
 
         <h2>Tableau associatif</h2>
         <?php
-            //Vider un tableau
-            $picture = [];
-            $picture = [
-                'src' => 'img/alicia_720.png',
-                'alt' => 'Jusqte la bosse enfait'
-            ];
+        //Vider un tableau
+        $picture = [];
+        $picture = [
+            'src' => 'img/alicia_720.png',
+            'alt' => 'Jusqte la bosse enfait'
+        ];
 
-            var_dump ($picture);
+        var_dump($picture);
         ?>
         <img src="<?= $picture['src'] ?>" alt="<?= $picture['alt'] ?>" width="200">
 
         <h2>Tableau à 2 dimensions : liste de factures</h2>
         <?php
-            // 2équivalent à : $invoices = [];
-            $invoices = array();
+        // 2équivalent à : $invoices = [];
+        $invoices = array();
 
 
-            //Tableau repésantant une seule facture 
-            $invoices = [
+        //Tableau repésantant une seule facture 
+        $invoices = [
+            'number' => '40',
+            'customer' => 'Tante Ursule',
+            'date' => '11-01-2020',
+            'amount' => '500',
+        ];
+
+        echo $invoices['amount'];
+
+        //Pluieurs factures dans une même varible
+        $invoices = [
+            [
                 'number' => '40',
                 'customer' => 'Tante Ursule',
                 'date' => '11-01-2020',
-                'amount' => '500',
-            ];
+                'amount' => '500'
+            ],
+            [
+                'number' => '41',
+                'customer' => 'Tante Agathe',
+                'date' => '12-01-2020',
+                'amount' => '800.50'
+            ],
+        ];
 
-            echo $invoices['amount'];
-
-            //Pluieurs factures dans une même varible
-            $invoices = [
-                [
-                    'number' => '40',
-                    'customer' => 'Tante Ursule',
-                    'date' => '11-01-2020',
-                    'amount' => '500'
-                ],
-                [
-                    'number' => '41',
-                    'customer' => 'Tante Agathe',
-                    'date' => '12-01-2020',
-                    'amount' => '800.50'
-                ],
-            ];
-
-            var_dump($invoices)
+        var_dump($invoices)
         ?>
 
         <table>
@@ -106,13 +108,13 @@ ini_set("display_errors", 1);
                 </tr>
             </thead>
             <tbody>
-                <?php for($compteur = 0; $compteur < count($invoices); $compteur++ ) : ?>
-                <tr>
-                    <td><?php echo $invoices[$compteur]['number']?> </td>
-                    <td><?php echo $invoices[$compteur]['date']?></td>
-                    <td><?php echo $invoices[$compteur]['customer']?></td>
-                    <td><?php echo $invoices[$compteur]['amount']?></td>
-                </tr>
+                <?php for ($compteur = 0; $compteur < count($invoices); $compteur++) : ?>
+                    <tr>
+                        <td><?php echo $invoices[$compteur]['number'] ?> </td>
+                        <td><?php echo $invoices[$compteur]['date'] ?></td>
+                        <td><?php echo $invoices[$compteur]['customer'] ?></td>
+                        <td><?php echo $invoices[$compteur]['amount'] ?></td>
+                    </tr>
                 <?php endfor; ?>
             </tbody>
         </table>
@@ -120,35 +122,35 @@ ini_set("display_errors", 1);
         <h2>La boucle foreach</h2>
         <?php
 
-            $invoices = [
-                [
-                    'number' => '40',
-                    'customer' => 'Tante Ursule',
-                    'date' => '11-01-2020',
-                    'amount' => '500'
-                ],
-                [
-                    'number' => '41',
-                    'customer' => 'Tante Agathe',
-                    'date' => '12-01-2020',
-                    'amount' => '800.50'
-                ],
-            ];
+        $invoices = [
+            [
+                'number' => '40',
+                'customer' => 'Tante Ursule',
+                'date' => '11-01-2020',
+                'amount' => '500'
+            ],
+            [
+                'number' => '41',
+                'customer' => 'Tante Agathe',
+                'date' => '12-01-2020',
+                'amount' => '800.50'
+            ],
+        ];
 
-            /*Pour chaque ligne du tableau $invoices
+        /*Pour chaque ligne du tableau $invoices
             L'indice est stockée dans $key 
             La valeur de la ligne(le 2eme taleau) est stocké dans $value
             */
 
 
-            foreach ($invoices as $key => $value) {
-                echo $key . '<br>';
-                var_dump($value);
-                //Equivalent à : $invoices[$compteur]['customer] de la boucle for
-                echo $value ['customer']. '<br>';
-            }
+        foreach ($invoices as $key => $value) {
+            echo $key . '<br>';
+            var_dump($value);
+            //Equivalent à : $invoices[$compteur]['customer] de la boucle for
+            echo $value['customer'] . '<br>';
+        }
 
-            /*Pour chaque ligne du tableau $invoices
+        /*Pour chaque ligne du tableau $invoices
             La valeur (donc, les données d'une facture) est stockée dans $invoices 
 
             Caculer 
@@ -159,14 +161,14 @@ ini_set("display_errors", 1);
                 TTC : montant TTC
             */
 
-            /*foreach ($invoices as $invoice) {
+        /*foreach ($invoices as $invoice) {
                 echo $invoice['amount']. '<br>';
             }*/
 
-            foreach ($invoices as $invoice) {
-                echo 'TVA : ' .$invoice['amount'] * 0.2 .'<br>';
-                echo 'TTC : ' .$invoice['amount'] * 1.2 . '<br>';
-            }
+        foreach ($invoices as $invoice) {
+            echo 'TVA : ' . $invoice['amount'] * 0.2 . '<br>';
+            echo 'TTC : ' . $invoice['amount'] * 1.2 . '<br>';
+        }
 
         ?>
 
@@ -182,18 +184,43 @@ ini_set("display_errors", 1);
                 </tr>
             </thead>
             <tbody>
-                <?php foreach($invoices as $invoice) : ?>
-                <tr> 
-                    <td><?=$invoice['number'] ?></td>
-                    <td><?=$invoice['customer'] ?></td>
-                    <td><?=$invoice['date'] ?></td>
-                    <td><?=$invoice['amount'] ?></td>
-                    <td><?=$invoice['amount'] * 0.2 ?></td>
-                    <td><?=$invoice['amount'] * 1.2 ?></td>
+                <?php foreach ($invoices as $invoice) : ?>
+                    <tr>
+                        <td><?= $invoice['number'] ?></td>
+                        <td><?= $invoice['customer'] ?></td>
+                        <td><?= $invoice['date'] ?></td>
+                        <td><?= $invoice['amount'] ?></td>
+                        <td><?= $invoice['amount'] * 0.2 ?></td>
+                        <td><?= $invoice['amount'] * 1.2 ?></td>
                     <?php endforeach; ?>
-                </tr>
+                    </tr>
             </tbody>
         </table>
+
+        <h2>Trier les valeurs</h2>
+
+        <?php
+            $input = ['w','a','c','f','e'] ;
+
+            var_dump($input);
+            //Trie les valeurs dans le tableau
+            sort($input);   
+            var_dump($input);
+        ?>
+
+        <h2>Supprimer une ligne dans un tableau</h2>
+
+        <?php 
+            //Supprimer la ligne dont la clé est 0
+            unset( $input[3]);
+            //Problème : Les clés restantes sont conserver et mais pas celle supprimer
+            var_dump($input);
+            //retourner ttes les valeurs dans un vouveau tableau : les clés sont recréer dans le nouveau tableau
+
+            $input2 = array_values($input);
+            var_dump($input2);
+        ?>
+
 
 
     </main>
@@ -204,4 +231,5 @@ ini_set("display_errors", 1);
         <a id="goTop" href="#" title="Haut de page">&UpArrow;</a>
     </footer>
 </body>
+
 </html>
